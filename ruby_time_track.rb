d25e958e191d39d1e02d888da1097c937dab4138
Nalipp <nateplipp@gmail.com>
2016-05-05 08:32:05 +0900

    improve methods

--- ruby_time_track.rb
+++ ruby_time_track.rb
@@ -1,5 +1,5 @@
 
-  @categories = { "ruby" => 300, "Rails" => 500, "Html" => 250, "JavaScript" => 150, "Computer Science" => 100 }
+  @categories = { "ruby" => 300, "rails" => 500, "html" => 250, "javascript" => 150, "computer science" => 100 }
 
   def start
     intro
@@ -18,25 +18,37 @@
     puts "Quit                  : Q"
   end
 
+  def get_categories(hash)
+    hash.each { |k,v| puts k.capitalize}
+  end
+
+  def get_time(hash, category)
+    hash.each { |k,v| puts "#{k}".capitalize + " : #{v}" if k == category}
+  end
+
+  def get_categories_time(hash)
+    hash.each { |k,v| puts "#{k}".capitalize + " : #{v}" }
+  end
+
   def read_category
     puts "Choose a category"
-    @categories.each { |k,v| puts k}
+    get_categories(@categories)
     category = gets.chomp.downcase
-    @categories.each { |k,v| puts "#{k} : #{v}" if k.downcase == category }
+    get_time(@categories, category)
   end
 
   def create_category
     puts "Please enter the category name"
-    new_category = gets.chomp
+    new_category = gets.chomp.downcase
     puts "please enter the time total"
-    time_total = gets.chomp
+    time_total = gets.chomp.downcase
     @categories[new_category] = time_total
     puts "time total has been added to the database"
   end
 
   def destroy_category
     puts "What category would you like to remove?"
-    @categories.each { |k,v| puts k}
+    get_categories(@categories)
     delete_category = gets.chomp.downcase
     @categories.delete_if { |k,v| k.downcase == delete_category.downcase}
     puts "#{delete_category} has been removed from the database"
@@ -44,19 +56,19 @@
 
   def update_category
     puts "What category would you like to update?"
-    @categories.each { |k,v| puts k}
-    update_time_total = gets.chomp
+    get_categories(@categories)
+    update_time_total = gets.chomp.downcase
     puts "Update category   : C"
     puts "Update time total : Z"
-    update_choice = gets.chomp
+    update_choice = gets.chomp.downcase
     if update_choice.downcase == "c"
       puts "Enter the new category name"
-      update_category = gets.chomp
+      update_category = gets.chomp.downcase
       @categories[update_category] = @categories.delete(update_time_total)
       puts "The category name has been updated in the database"
     elsif update_choice.downcase == "z"
       puts "Enter the new time total"
-      update_time_total = gets.chomp
+      update_time_total = gets.chomp.downcase
       @categories[update_category] = update_time_total
       puts "The time total has been updated in the database"
     else
@@ -80,7 +92,7 @@
       elsif prompt == "u"
         update_category
       elsif prompt == "l"
-        @categories.each { |k,v| puts "#{k} : #{v}" }
+        get_categories_time(@categories)
       elsif prompt == "q"
         puts "goodbye"
         break
