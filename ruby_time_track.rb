eb6682b6bb5678e41fc01905a73f8a19f32a09fc
Nalipp <nateplipp@gmail.com>
2016-05-04 17:59:58 +0900

    add methods

--- ruby_time_track.rb
+++ ruby_time_track.rb
@@ -1,38 +1,50 @@
-categories = { "ruby" => 300, "Rails" => 500, "Html" => 250, "JavaScript" => 150, "Computer Science" => 100 }
 
-puts "*******************"
-puts "How I learn to code"
-puts "*******************"
-puts "Create a new category : C"
-puts "Read a category       : R"
-puts "Update a category     : U"
-puts "Destroy a category    : D"
-puts "List all categories   : L"
-puts "Quit                  : Q"
+  @categories = { "ruby" => 300, "Rails" => 500, "Html" => 250, "JavaScript" => 150, "Computer Science" => 100 }
 
-loop do
-  prompt = gets.chomp.downcase
-  if prompt == "r"
+  def start
+    intro
+    program
+  end
+
+  def intro
+    puts "*******************"
+    puts "How I learn to code"
+    puts "*******************"
+    puts "Create a new category : C"
+    puts "Read a category       : R"
+    puts "Update a category     : U"
+    puts "Destroy a category    : D"
+    puts "List all categories   : L"
+    puts "Quit                  : Q"
+  end
+
+  def read_category
     puts "Choose a category"
-    categories.each { |k,v| puts k}
+    @categories.each { |k,v| puts k}
     category = gets.chomp.downcase
-    categories.each { |k,v| puts "#{k} : #{v}" if k.downcase == category }
-  elsif prompt == "c"
+    @categories.each { |k,v| puts "#{k} : #{v}" if k.downcase == category }
+  end
+
+  def create_category
     puts "Please enter the category name"
     new_category = gets.chomp
     puts "please enter the time total"
     time_total = gets.chomp
-    categories[new_category] = time_total
-    puts "time_total has been added to the database"
-  elsif prompt == "d"
+    @categories[new_category] = time_total
+    puts "time total has been added to the database"
+  end
+
+  def destroy_category
     puts "What category would you like to remove?"
-    categories.each { |k,v| puts k}
-    delete_category = gets.chomp
-    categories.delete_if { |k,v| k.downcase == delete_category.downcase}
+    @categories.each { |k,v| puts k}
+    delete_category = gets.chomp.downcase
+    @categories.delete_if { |k,v| k.downcase == delete_category.downcase}
     puts "#{delete_category} has been removed from the database"
-  elsif prompt == "u"
+  end
+
+  def update_category
     puts "What category would you like to update?"
-    categories.each { |k,v| puts k}
+    @categories.each { |k,v| puts k}
     update_time_total = gets.chomp
     puts "Update category   : C"
     puts "Update time total : Z"
@@ -40,22 +52,42 @@ loop do
     if update_choice.downcase == "c"
       puts "Enter the new category name"
       update_category = gets.chomp
-      categories[update_category] = categories.delete(update_time_total)
+      @categories[update_category] = @categories.delete(update_time_total)
       puts "The category name has been updated in the database"
     elsif update_choice.downcase == "z"
       puts "Enter the new time total"
       update_time_total = gets.chomp
-      categories[update_category] = update_time_total
+      @categories[update_category] = update_time_total
       puts "The time total has been updated in the database"
     else
-      puts "That is not a valid option"
+      invalid_option
     end
-  elsif prompt == "l"
-    categories.each { |k,v| puts "#{k} : #{v}" }
-  elsif prompt == "q"
-    puts "goodbye"
-    break
-  else
+  end
+
+  def invalid_option
     puts "That is not a valid option"
   end
-end
+
+  def program
+    loop do
+      prompt = gets.chomp.downcase
+      if prompt == "r"
+        read_category
+      elsif prompt == "c"
+        create_category
+      elsif prompt == "d"
+        destroy_category
+      elsif prompt == "u"
+        update_category
+      elsif prompt == "l"
+        @categories.each { |k,v| puts "#{k} : #{v}" }
+      elsif prompt == "q"
+        puts "goodbye"
+        break
+      else
+        invalid_option
+      end
+    end
+  end
+
+start
